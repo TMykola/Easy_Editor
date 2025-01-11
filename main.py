c4f1c6a487b78a0a0a64e582dee14a16e890aa20
TMykola <milolatarasuk@gmail.com>
2025-01-11 13:31:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,11 +12,18 @@ def filter(all_obj):
                 result.append(file)
     return result
 
+def error(txt):
+    mb = QMessageBox()
+    mb.setText(txt)
+    mb.exec_()
+
 def choose_workdir():
     global workdir
     workdir = QFileDialog.getExistingDirectory()
-    photo_list.addItems(filter(os.listdir()))
-
+    try:
+        photo_list.addItems(filter(os.listdir(workdir)))
+    except:
+        error("Спочатку виберіть папку де наявні фотографії")
 def show_photo():
     global name_photo
     name_photo = photo_list.currentItem().text()
@@ -24,49 +31,63 @@ def show_photo():
     photo_label.setPixmap(photo.scaled(WIDTH,HEIGHT, aspectRatioMode=Qt.KeepAspectRatio))
 
 def make_left():
-    abs_path_photo = os.path.join(workdir,name_photo)
-    with Image.open(abs_path_photo) as photo_obj:
-        photo_left = photo_obj.transpose(Image.ROTATE_90)
-        new_abs_path_photo = os.path.join(workdir, "Image", "left_" + name_photo)
-        photo_left.save(new_abs_path_photo)
-    photo.load(new_abs_path_photo)
-    photo_label.setPixmap(photo.scaled(WIDTH,HEIGHT,aspectRatioMode=Qt.KeepAspectRatio))
-        
+    try:
+        abs_path_photo = os.path.join(workdir,name_photo)
+        with Image.open(abs_path_photo) as photo_obj:
+            photo_left = photo_obj.transpose(Image.ROTATE_90)
+            new_abs_path_photo = os.path.join(os.path.abspath(__file__ + "/.."), "Image", "left_" + name_photo)
+            photo_left.save(new_abs_path_photo)
+        photo.load(new_abs_path_photo)
+        photo_label.setPixmap(photo.scaled(WIDTH,HEIGHT,aspectRatioMode=Qt.KeepAspectRatio))
+    except:
+        error("Спочатку виберіть фотографію")   
 def make_right():
-    abs_path_photo = os.path.join(workdir,name_photo)
-    with Image.open(abs_path_photo) as photo_obj:
-        photo_right = photo_obj.transpose(Image.ROTATE_270)
-        new_abs_path_photo = os.path.join(workdir, "Image", "right_" + name_photo)
-        photo_right.save(new_abs_path_photo)
-    photo.load(new_abs_path_photo)
-    photo_label.setPixmap(photo.scaled(WIDTH,HEIGHT,aspectRatioMode=Qt.KeepAspectRatio))
+    try:
+        abs_path_photo = os.path.join(workdir,name_photo)
+        with Image.open(abs_path_photo) as photo_obj:
+            photo_right = photo_obj.transpose(Image.ROTATE_270)
+            new_abs_path_photo = os.path.join(os.path.abspath(__file__ + "/.."), "Image", "right_" + name_photo)
+            photo_right.save(new_abs_path_photo)
+        photo.load(new_abs_path_photo)
+        photo_label.setPixmap(photo.scaled(WIDTH,HEIGHT,aspectRatioMode=Qt.KeepAspectRatio))
+    except:
+        error("Спочатку виберіть фотографію")   
 
 def make_miror():
-    abs_path_photo = os.path.join(workdir,name_photo)
-    with Image.open(abs_path_photo) as photo_obj:
-        photo_miror = photo_obj.transpose(Image.FLIP_LEFT_RIGHT)
-        new_abs_path_photo = os.path.join(workdir, "Image", "miror_" + name_photo)
-        photo_miror.save(new_abs_path_photo)
-    photo.load(new_abs_path_photo)
-    photo_label.setPixmap(photo.scaled(WIDTH,HEIGHT,aspectRatioMode=Qt.KeepAspectRatio))
+    try:
+        abs_path_photo = os.path.join(workdir,name_photo)
+        with Image.open(abs_path_photo) as photo_obj:
+            photo_miror = photo_obj.transpose(Image.FLIP_LEFT_RIGHT)
+            new_abs_path_photo = os.path.join(os.path.abspath(__file__ + "/.."), "Image", "miror_" + name_photo)
+            photo_miror.save(new_abs_path_photo)
+        photo.load(new_abs_path_photo)
+        photo_label.setPixmap(photo.scaled(WIDTH,HEIGHT,aspectRatioMode=Qt.KeepAspectRatio))
+    except:
+        error("Спочатку виберіть фотографію")   
 
 def make_black_white():
-    abs_path_photo = os.path.join(workdir,name_photo)
-    with Image.open(abs_path_photo) as photo_obj:
-        photo_BW = photo_obj.convert("L")
-        new_abs_path_photo = os.path.join(workdir, "Image", "BW_" + name_photo)
-        photo_BW.save(new_abs_path_photo)
-    photo.load(new_abs_path_photo)
-    photo_label.setPixmap(photo.scaled(WIDTH,HEIGHT,aspectRatioMode=Qt.KeepAspectRatio))
+    try:
+        abs_path_photo = os.path.join(workdir,name_photo)
+        with Image.open(abs_path_photo) as photo_obj:
+            photo_BW = photo_obj.convert("L")
+            new_abs_path_photo = os.path.join(os.path.abspath(__file__ + "/.."), "Image", "BW_" + name_photo)
+            photo_BW.save(new_abs_path_photo)
+        photo.load(new_abs_path_photo)
+        photo_label.setPixmap(photo.scaled(WIDTH,HEIGHT,aspectRatioMode=Qt.KeepAspectRatio))
+    except:
+        error("Спочатку виберіть фотографію")   
 
 def make_blur():
-    abs_path_photo = os.path.join(workdir,name_photo)
-    with Image.open(abs_path_photo) as photo_obj:
-        photo_blur = photo_obj.filter(ImageFilter.GaussianBlur(10))
-        new_abs_path_photo = os.path.join(workdir, "Image", "BW_" + name_photo)
-        photo_blur.save(new_abs_path_photo)
-    photo.load(new_abs_path_photo)
-    photo_label.setPixmap(photo.scaled(WIDTH,HEIGHT,aspectRatioMode=Qt.KeepAspectRatio))
+    try:
+        abs_path_photo = os.path.join(workdir,name_photo)
+        with Image.open(abs_path_photo) as photo_obj:
+            photo_blur = photo_obj.filter(ImageFilter.GaussianBlur(Blur_range.value()))
+            new_abs_path_photo = os.path.join(os.path.abspath(__file__ + "/.."), "Image", "BW_" + name_photo)
+            photo_blur.save(new_abs_path_photo)
+        photo.load(new_abs_path_photo)
+        photo_label.setPixmap(photo.scaled(WIDTH,HEIGHT,aspectRatioMode=Qt.KeepAspectRatio))
+    except:
+        error("Спочатку виберіть фотографію")   
 
 blur.clicked.connect(make_blur)
 black_white.clicked.connect(make_black_white)
